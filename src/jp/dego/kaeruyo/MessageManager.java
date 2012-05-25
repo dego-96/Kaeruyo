14b242f5a7ff57d60498269edfd50da204b36605
dego <dego.desu@gmail.com>
2012-05-25 16:25:52 +0900

    帰宅時間の表示を修正

--- src/jp/dego/kaeruyo/MessageManager.java
+++ src/jp/dego/kaeruyo/MessageManager.java
@@ -4,23 +4,23 @@ import java.util.Date;
 
 public class MessageManager
 {
-//    public static String getMessage(KitakuInfo info)
-//    {
-//        String res = "";
-//        int mt = info.getMoveTime();
-//        Date date = new Date(System.currentTimeMillis() + mt * 60 * 1000);
-//        String gethome = date.getHours() + "時" + date.getMinutes() + "分";
-//        if (info.isMMS()) {
-//            // MMSの場合
-//            res = info.getMessage().replaceAll(KitakuInfo.HHMM, gethome);
-//        } else {
-//            // SMSの場合
-//            res = info.getSubject() + info.getMessage();
-//            res = res.replace(KitakuInfo.HHMM, gethome);
-//        }
-//
-//        return res;
-//    }
+    // public static String getMessage(KitakuInfo info)
+    // {
+    // String res = "";
+    // int mt = info.getMoveTime();
+    // Date date = new Date(System.currentTimeMillis() + mt * 60 * 1000);
+    // String gethome = date.getHours() + "時" + date.getMinutes() + "分";
+    // if (info.isMMS()) {
+    // // MMSの場合
+    // res = info.getMessage().replaceAll(KitakuInfo.HHMM, gethome);
+    // } else {
+    // // SMSの場合
+    // res = info.getSubject() + info.getMessage();
+    // res = res.replace(KitakuInfo.HHMM, gethome);
+    // }
+    //
+    // return res;
+    // }
 
     public static String getMessage(KitakuInfo info, boolean about)
     {
@@ -30,7 +30,7 @@ public class MessageManager
         int hh = date.getHours();
         int mm = date.getMinutes();
         if (about) {
-            if (info.getMoveTime() < 60) {
+            if (mt < 60) {
                 if (mm % 5 < 5)
                     mm = mm - (mm % 5) + 5;
                 else
@@ -39,8 +39,17 @@ public class MessageManager
                 if (mm % 10 != 0)
                     mm = mm - (mm % 10) + 10;
             }
+            if (mm >= 60) {
+                hh++;
+                mm -= 60;
+            }
         }
-        String gethome = hh + "時" + mm + "分";
+        String gethome;
+        if(mm != 0){
+            gethome = hh + "時" + mm + "分";
+        } else {
+            gethome = hh + "時";
+        }
         if (info.isMMS()) {
             // MMSの場合
             res = info.getMessage().replaceAll(KitakuInfo.HHMM, gethome);
@@ -55,9 +64,9 @@ public class MessageManager
 
     private static int PositionToMinute(int pos)
     {
-        if(-1 < pos && pos < 10)
+        if (-1 < pos && pos < 10)
             return 10 + pos * 5;
-        else if(pos < 17)
+        else if (pos < 17)
             return 60 + (pos - 10) * 10;
         else
             return 0;
